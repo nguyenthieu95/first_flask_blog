d714bb71db1d235aad1a828cf165399e92f76e9f
ThieuNv <nguyenthieu2102@gmail.com>
2018-06-15 17:14:55 +0700

    Dev, user authentication and database

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,7 +1,14 @@
 from datetime import datetime
-from flaskblog import db
+from flaskblog import db, login_manager
+from flask_login import UserMixin
 
-class User(db.Model):
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(24), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -10,7 +17,8 @@ class User(db.Model):
     posts = db.relationship('Post', backref="author", lazy=True)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+        return u"User('{self.username}', '{self.email}', '{self.image_file}')"
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -20,5 +28,5 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
     def __repr__(self):
-        return f"User('{self.title}', '{self.date_posted}')"
+        return u"User('{self.title}', '{self.date_posted}')"
 
