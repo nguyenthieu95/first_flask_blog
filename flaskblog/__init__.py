e8121bbe4e93a3be367051d9fc65c621e3998269
ThieuNv <nguyenthieu2102@gmail.com>
2018-06-20 01:59:56 +0700

    Dev, Blueprints and Configuration - Restructure project

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,26 +1,39 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flaskblog.config import Config
 
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'cd9a5b64528d72d7420e1315ddf03617'
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
-db = SQLAlchemy(app)    # database instances
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'              # function name of route
+### Extension object created outside of function make importable to another package
+db = SQLAlchemy()                            # database instances
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'        # function name of route
 login_manager.login_message_category = 'info'   # message color for bootstrap when logged in.
-
-app.config['MAIL_SERVER'] = "smtp.gmail.com"
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = os.environ.get("EMAIL_USER")
-app.config['MAIL_PASSWORD'] = os.environ.get("EMAIL_PASS")
-mail = Mail(app)
+mail = Mail()
 
 
-from flaskblog import routes
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    ### Init extension object inside creation application
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    ### Blueprint
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
+
+
